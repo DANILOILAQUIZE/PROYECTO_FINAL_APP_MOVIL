37afca9bb99d40d087c670b0fccf2c3751a60e1d
Sali2003-alt <154335113+Sali2003-alt@users.noreply.github.com>
2025-07-22 23:23:26 -0500

    materia_dart

--- lib/entities/materia_entity.dart
+++ lib/entities/materia_entity.dart
@@ -1,41 +1,43 @@
-class Materia {
+class MateriaEntity {
   int? id;
   String nombre;
   int codigo;
   String descripcion;
   int horas;
   String semestre;
+  int fkPeriodoId;
 
-  Materia({
+  MateriaEntity({
     this.id,
     required this.nombre,
     required this.codigo,
     required this.descripcion,
     required this.horas,
     required this.semestre,
+    required this.fkPeriodoId,
   });
 
-  // Convierte la instancia de Materia en un mapa (útil para bases de datos o APIs)
-  Map<String, dynamic> toMap() {
-    return {
-      "id": id,
-      "nombre": nombre,
-      "codigo": codigo,
-      "descripcion": descripcion,
-      "horas": horas,
-      "semestre": semestre,
-    };
+  factory MateriaEntity.fromMap(Map<String, dynamic> map) {
+    return MateriaEntity(
+      id: map['id'],
+      nombre: map['nombre'],
+      codigo: map['codigo'],
+      descripcion: map['descripcion'],
+      horas: map['horas'],
+      semestre: map['semestre'],
+      fkPeriodoId: map['fk_periodo_id'],
+    );
   }
 
-  // Crea una instancia de Materia a partir de un mapa
-  factory Materia.fromMap(Map<String, dynamic> data) {
-    return Materia(
-      id: data["id"],
-      nombre: data["nombre"],
-      codigo: data["codigo"],
-      descripcion: data["descripcion"],
-      horas: data["horas"],
-      semestre: data["semestre"],
-    );
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nombre': nombre,
+      'codigo': codigo,
+      'descripcion': descripcion,
+      'horas': horas,
+      'semestre': semestre,
+      'fk_periodo_id': fkPeriodoId,
+    };
   }
 }
--- lib/repositories/materia_repository.dart
+++ lib/repositories/materia_repository.dart
@@ -3,31 +3,36 @@ import '../settings/db_connection.dart';
 
 class MateriaRepository {
   static String tableName = "materia";
-  static Future<int> insert(Materia materia) async {
+
+  static Future<int> insert(MateriaEntity materia) async {
     return await DBConnection.insert(tableName, materia.toMap());
   }
 
-  static Future<int> update(Materia materia) async {
-    return await DBConnection.update(
-      tableName,
-      materia.toMap(),
-      materia.id as int,
-    );
+  static Future<int> update(MateriaEntity materia) async {
+    return await DBConnection.update(tableName, materia.toMap(), materia.id!);
   }
 
-  static Future<int> delete(Materia materia) async {
-    return await DBConnection.delete(tableName, materia.id as int);
+  static Future<int> delete(MateriaEntity materia) async {
+    return await DBConnection.delete(tableName, materia.id!);
   }
 
-  static Future<List<Materia>> list() async {
-    var result = await DBConnection.list(tableName);
-    if (result.isEmpty) {
-      return List.empty();
-    } else {
-      return List.generate(
-        result.length,
-        (index) => Materia.fromMap(result[index]),
-      );
+  static Future<List<MateriaEntity>> list() async {
+    final result = await DBConnection.list(tableName);
+    return result.map((item) => MateriaEntity.fromMap(item)).toList();
+  }
+
+  static Future<MateriaEntity?> getById(int id) async {
+    final result = await DBConnection.filter(tableName, 'id = ?', [id]);
+    if (result.isNotEmpty) {
+      return MateriaEntity.fromMap(result.first);
     }
+    return null;
+  }
+
+  static Future<List<MateriaEntity>> getByPeriodoId(int periodoId) async {
+    final result = await DBConnection.filter(tableName, 'fk_periodo_id = ?', [
+      periodoId,
+    ]);
+    return result.map((item) => MateriaEntity.fromMap(item)).toList();
   }
 }
